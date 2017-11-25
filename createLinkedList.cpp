8c89ce8ee9c3a4520bf145b6f38ab76f35581339
s-ab-anand <anandshocking007@gmail.com>
2017-11-24 21:17:54 -0600

    linked list

---
+++ createLinkedList.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include "node.h"
+using namespace std;
+
+class MyLinkedList{
+  private:
+    struct Node* head;
+  public:
+    MyLinkedList(){
+      head = NULL;
+    }
+    struct Node* addNode(int val){
+      Node* myNode = new Node();
+      myNode->val = val;
+      myNode->next = NULL;
+      head = myNode;
+      return head;
+    }
+    void printList(){
+      struct Node* start;
+      start = head;
+      while(start){
+        cout<<start->val<<"\t";
+        start = start->next;
+      }
+      cout<<"\n";
+    }
+
+};
+int main(int argc, char* argv[])
+{
+  MyLinkedList myList;
+  myList.addNode(1);
+  myList.printList();
+}
---
+++ node.h
@@ -0,0 +1,9 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#endif
+#include<new>
+
+struct Node{
+  int val;
+  struct Node* next;
+};
