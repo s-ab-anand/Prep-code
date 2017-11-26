5c3e3a7dafb7748faf484453575bc02c849c0799
s-ab-anand <anandshocking007@gmail.com>
2017-11-25 18:13:28 -0600

    some changes

--- createLinkedList.cpp
+++ createLinkedList.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 #include<new>
 #include<cstdlib>
+#include<string>
+#include<sstream>
 #include "node.h"
 using namespace std;
 
@@ -9,37 +11,68 @@ class MyLinkedList{
   private:
     struct Node* head;
   public:
-    MyLinkedList(){
-      head = NULL;
-    }
-    struct Node* addNode(int val){
-      Node* myNode = new Node();
-      myNode->val = val;
-      myNode->next = NULL;
-      if (head){
-        myNode->next = head;
-        head = myNode;
-        return head;
-      } else {
-        head = myNode;
-        return head;
-      }
-    }
-    void printList(){
-      struct Node* start;
-      start = head;
-      while(start){
-        cout<<start->val<<"\t";
-        start = start->next;
-      }
-      cout<<"\n";
-    }
-
+    MyLinkedList();
+    struct Node* addNode(int val);
+    struct Node* addNodefromFile(MyLinkedList);
+    void printList();
 };
+//member function declarations
+MyLinkedList::MyLinkedList()
+{
+  head = NULL;
+}
+struct Node* MyLinkedList::addNode(int val)
+{
+  Node* myNode = new Node();
+  myNode->val = val;
+  myNode->next = NULL;
+  if (head){
+    myNode->next = head;
+    head = myNode;
+    return head;
+  } else {
+    head = myNode;
+    return head;
+  }
+}
+void MyLinkedList::printList(){
+  struct Node* start;
+  start = head;
+  while(start){
+    cout<<start->val<<"\t";
+    start = start->next;
+  }
+  cout<<"\n";
+}
+void readinFile(MyLinkedList* obj)
+{
+  ifstream myFile ("arrInt_100.txt");
+  string line; int num = 0;
+  if (myFile.is_open()){
+    while(getline(myFile, line, ',')){
+      stringstream sobj(line);
+      sobj >> num;
+      (*obj).addNode(num);
+    }
+  }
+}
+// Main function
 int main(int argc, char* argv[])
 {
   MyLinkedList myList;
-  myList.addNode(1);
-  myList.addNode(2);
+  //myList.addNode(1);
+  //myList.addNode(2);
+
+  // creating a list of 100 nodes
+  readinFile(&myList);
   myList.printList();
+
+  // values of the first, second and third quaters of the list
+  cout<<getNodeNum(&myList, 25)<<"\n";
+  cout<<getNodeNum(&myList, 50)<<"\n";
+  cout<<getNodeNum(&myList, 100)<"\n";
+
+
+  // search for a value in the linked list
+
 }
